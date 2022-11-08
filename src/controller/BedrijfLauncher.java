ff755c77e1f13a4f9117f225079d97723e423f77
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2022-11-08 10:05:20 +0100

    Hoofdstuk 4

--- src/controller/BedrijfLauncher.java
+++ src/controller/BedrijfLauncher.java
@@ -3,6 +3,7 @@ package controller;
 import model.Afdeling;
 import model.Persoon;
 import model.Werknemer;
+import model.Zzper;
 
 /**
  * @author Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
@@ -19,17 +20,25 @@ public class BedrijfLauncher {
         afdelingen[2] = new Afdeling("Management", "Almere");
         afdelingen[3] = new Afdeling("Documentatie", "Gouda");
 
-        System.out.println(afdelingen[1]);
-
         Werknemer baas = new Werknemer("Mark", "Den Haag", afdelingen[2], 10000);
         Werknemer medewerker = new Werknemer("Caroline", "Delft", afdelingen[1], 4000);
-        Persoon assistent = new Persoon("Klaas");
+        Zzper assistent = new Zzper("Klaas", "Diemen", afdelingen[3], 50.0);
+
+        assistent.huurIn(160);
 
         System.out.printf("Het aantal personen in het bedrijf is %d%n", Persoon.aantalPersonen);
+
+        System.out.println();
 
         System.out.println(baas);
         System.out.println(medewerker);
         System.out.println(assistent);
+
+        System.out.println();
+
+        System.out.printf("%s verdient %.2f per jaar\n", baas.getNaam(), baas.berekenJaarInkomen());
+        System.out.printf("%s verdient %.2f per jaar\n", medewerker.getNaam(), medewerker.berekenJaarInkomen());
+        System.out.printf("%s verdient %.2f per jaar\n", assistent.getNaam(), assistent.berekenJaarInkomen());
     }
 
 }
---
+++ src/model/Zzper.java
@@ -0,0 +1,33 @@
+package model;
+
+/**
+ * @author Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
+ * <p>
+ * Houdt de eigenschappen bij van een als zzper ingehuurd persoon
+ */
+public class Zzper extends Persoon {
+    private static final int DEFAULT_UREN_GEWERKT = 0;
+
+    private double uurTarief;
+    private int urenGewerkt;
+
+    public Zzper(String naam, String woonplaats, Afdeling afdeling, double uurTarief) {
+        super(naam, woonplaats, afdeling);
+        this.uurTarief = uurTarief;
+        this.urenGewerkt = DEFAULT_UREN_GEWERKT;
+    }
+
+    public void huurIn(int uren) {
+        urenGewerkt += uren;
+    }
+
+    @Override
+    public double berekenJaarInkomen() {
+        return urenGewerkt * uurTarief;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s en is een zzp-er met een uurtarief van %.2f", super.toString(), this.uurTarief);
+    }
+}
